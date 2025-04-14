6d7172fe282565c363ede554e728f7c57c5ddf8e
Teshome Abebe <161710654+tshabebe@users.noreply.github.com>
2025-04-14 19:59:40 +0000

    feat: more components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,27 @@
 import { Modal } from "@/primitives/Modal";
 import { Button } from "./button";
 import { AlertDialog } from "@/primitives/AlertDialog";
-import { DialogTrigger } from "react-aria-components";
+import { DialogTrigger, DisclosurePanel, MenuTrigger } from "react-aria-components";
 import { Autocomplete, AutocompleteItem } from "@/primitives/AutoComplete";
 import { Breadcrumb, Breadcrumbs } from "@/primitives/BreadCrumbs";
-import { Calendar } from "@/primitives/Calender";
+import { Calendar } from "@/primitives/Calendar";
 import { Checkbox, CheckboxGroup } from "@/primitives/CheckBox";
 import { ComboBox, ComboBoxItem } from "@/primitives/ComboBox";
+import { DateField } from "@/primitives/DateField";
+import { DatePicker } from "@/primitives/DatePicker";
+import { DateRangePicker } from "@/primitives/DateRangePicker";
+import {
+  Disclosure,
+  DisclosureGroup,
+  DisclosureHeader,
+} from "@/primitives/Disclosure";
+import { GridList, GridListItem } from "@/primitives/GridList";
+import { Link } from "@/primitives/Link";
+import { ListBox, ListBoxItem } from "@/primitives/ListBox";
+import { MoreHorizontal } from "lucide-react";
+import { Menu, MenuItem, MenuSeparator } from "@/primitives/Menu";
+import { Meter } from "@/primitives/Meter";
+import { NumberField } from "@/primitives/NumberField";
 
 export default function Home() {
   return (
@@ -20,72 +35,172 @@ export default function Home() {
       <CheckBoxExample />
       <CheckBOxGroupExample />
       <ComboBoxExample />
+      <DateFieldExample />
+      <DatePickerExample />
+      <DateRangePickerExample />
+      <DisclosureExample />
+      <DisclosueGroup />
+      <GridListExample />
+      <LinkExample />
+      <ListBoxExample />
+      <MenuExample />
+      <MeterExample />
+      <NumberFieldExaple />
     </div>
   );
 }
-function ComboBoxExample() {
-  return <ComboBox
+function MeterExample() {
+  return <Meter label="Storage Space" value={89} />
+}
+function MenuExample() {
+  return   <MenuTrigger>
+  <Button className="px-2">
+    <MoreHorizontal className="w-5 h-5" />
+  </Button>
+  <Menu>
+    <MenuItem id="new">New…</MenuItem>
+    <MenuItem id="open">Open…</MenuItem>
+    <MenuSeparator />
+    <MenuItem id="save">Save</MenuItem>
+    <MenuItem id="saveAs">Save as…</MenuItem>
+    <MenuSeparator />
+    <MenuItem id="print">Print…</MenuItem>
+  </Menu>
+</MenuTrigger>
+
+}
+function LinkExample() {
+  return <Link href="#"> the missing link</Link>
+}
+function ListBoxExample() {
+  return <ListBox
+  aria-label="Ice cream flavor"
+  selectionMode="multiple"
 >
-  <ComboBoxItem>
+  <ListBoxItem id="chocolate">
     Chocolate
-  </ComboBoxItem>
-  <ComboBoxItem id="mint">
+  </ListBoxItem>
+  <ListBoxItem id="mint">
     Mint
-  </ComboBoxItem>
-  <ComboBoxItem>
+  </ListBoxItem>
+  <ListBoxItem id="strawberry">
     Strawberry
-  </ComboBoxItem>
-  <ComboBoxItem>
+  </ListBoxItem>
+  <ListBoxItem id="vanilla">
     Vanilla
-  </ComboBoxItem>
-</ComboBox>
+  </ListBoxItem>
+</ListBox>
+}
+function NumberFieldExaple() {
+  return <NumberField label="Cookies"/>
+}
+function GridListExample() {
+  return (
+    <GridList
+      aria-label="Ice cream flavors"
+      onAction={undefined}
+      onScroll={() => {}}
+      onSelectionChange={() => {}}
+      selectionMode="multiple"
+    >
+      <GridListItem id="chocolate">Chocolate</GridListItem>
+      <GridListItem id="mint">Mint</GridListItem>
+      <GridListItem id="strawberry">Strawberry</GridListItem>
+      <GridListItem id="vanilla">Vanilla</GridListItem>
+    </GridList>
+  );
+}
+function DisclosueGroup() {
+  return (
+    <DisclosureGroup
+      onExpandedChange={() => {
+        console.log("changed");
+      }}
+    >
+      <Disclosure>
+        <DisclosureHeader>Files</DisclosureHeader>
+        <DisclosurePanel>Files content</DisclosurePanel>
+      </Disclosure>
+      <Disclosure>
+        <DisclosureHeader>Images</DisclosureHeader>
+        <DisclosurePanel>Images content</DisclosurePanel>
+      </Disclosure>
+      <Disclosure>
+        <DisclosureHeader>Documents</DisclosureHeader>
+        <DisclosurePanel>Documents content</DisclosurePanel>
+      </Disclosure>
+    </DisclosureGroup>
+  );
+}
+function DisclosureExample() {
+  return (
+    <Disclosure
+      onExpandedChange={(state) => {
+        console.log(state);
+      }}
+    >
+      <DisclosureHeader>Files</DisclosureHeader>
+      <DisclosurePanel>Files content</DisclosurePanel>
+    </Disclosure>
+  );
+}
+function DateRangePickerExample() {
+  return <DateRangePicker label="Trip Dates" />;
+}
+function DatePickerExample() {
+  return <DatePicker label="Event Date" />;
+}
+function DateFieldExample() {
+  return <DateField label="Event date" />;
+}
+function ComboBoxExample() {
+  return (
+    <ComboBox>
+      <ComboBoxItem>Chocolate</ComboBoxItem>
+      <ComboBoxItem id="mint">Mint</ComboBoxItem>
+      <ComboBoxItem>Strawberry</ComboBoxItem>
+      <ComboBoxItem>Vanilla</ComboBoxItem>
+    </ComboBox>
+  );
 }
 function CheckBOxGroupExample() {
-  return <CheckboxGroup
-  label="Cities"
->
-  <>
-    <Checkbox value="sf">
-      San Francisco
-    </Checkbox>
-    <Checkbox value="ny">
-      New York
-    </Checkbox>
-    <Checkbox value="sydney">
-      Sydney
-    </Checkbox>
-    <Checkbox value="london">
-      London
-    </Checkbox>
-    <Checkbox value="tokyo">
-      Tokyo
-    </Checkbox>
-  </>
-</CheckboxGroup>
+  return (
+    <CheckboxGroup label="Cities">
+      <>
+        <Checkbox value="sf">San Francisco</Checkbox>
+        <Checkbox value="ny">New York</Checkbox>
+        <Checkbox value="sydney">Sydney</Checkbox>
+        <Checkbox value="london">London</Checkbox>
+        <Checkbox value="tokyo">Tokyo</Checkbox>
+      </>
+    </CheckboxGroup>
+  );
 }
 function CheckBoxExample() {
-  return <Checkbox
->
-  Checkbox
-</Checkbox>
+  return <Checkbox>Checkbox</Checkbox>;
 }
 function CalenderExample() {
-  return <Calendar
-  aria-label="Event date"
-  onChange={function Ma(){}}
-  onFocusChange={function Ma(){}}
-/>
+  return (
+    <Calendar
+      aria-label="Event date"
+      onChange={function Ma() {}}
+      onFocusChange={function Ma() {}}
+    />
+  );
 }
 
 function BreadCrumbsExample() {
-  return   <Breadcrumbs onAction={() => {
-    console.log("navigated")
-  } }>
-  <Breadcrumb href="/">Home</Breadcrumb>
-  <Breadcrumb href="/react-aria">React Aria</Breadcrumb>
-  <Breadcrumb>Breadcrumbs</Breadcrumb>
-</Breadcrumbs>
-
+  return (
+    <Breadcrumbs
+      onAction={() => {
+        console.log("navigated");
+      }}
+    >
+      <Breadcrumb href="/">Home</Breadcrumb>
+      <Breadcrumb href="/react-aria">React Aria</Breadcrumb>
+      <Breadcrumb>Breadcrumbs</Breadcrumb>
+    </Breadcrumbs>
+  );
 }
 function AutoCompleteExample() {
   return (
